bf3d9d321bae4445a3c9edcfac2135f56f0e7856
Eliseu <eliseru45@gmail.com>
2026-09-24 10:36:29 -0300

    Código pronto de forma bruta, resta lapidar, talvez criar interface básica, favor revisar todo o código, fazer vários testes

--- main.py
+++ main.py
@@ -31,17 +31,33 @@ plt.ylabel("Verdadeiro/Falso")
 plt.show()                                          #Cria gráfico de sinais.
 
 
-tquebra = fs * 0.03                                 #declara porcentagem usada para agrupar
+tquebra = fs * 0.03                                 #declara microsegundos convertidos para amostras
 indice = np.where(sinal)[0]                         #Cria array onde apenas foi denotado true no array sinal 
 quebra = np.where(np.diff(indice)>tquebra)[0]+1     #Identifica todas as quebras, separando de fato o silencio das batidas atraves da tquebra
 batidas = np.split(indice, quebra)                  #Cria os grupos com indices declarando assim onde há inicio de uma batida e termino de cada uma
 
-batida_inicio = []
+batida_inicio = []                                 
 batida_fim = []
 
-for i in batidas:
-    batida_inicio.append(i[0])
-    batida_fim.append(i[-1])
+for i in batidas:   
+    batida_inicio.append(i[0])                      #guarda no array o inicio de cada batida no array de inicio
+    batida_fim.append(i[-1])                        #guarda no array o termino de cada batida
 
-intervalo = 20000
+batida_inicio = np.array(batida_inicio)             
+batida_fim = np.array(batida_fim)
+
+silencio = batida_inicio[1:] - batida_fim[:-1]
+
+intervalo = 20000                                   #threshold que decide se é batida dupla ou singular
 resultado = []
+diferente = np.where(silencio > intervalo)[0] + 1   #Encontra sempre que silencio ocorre
+posicoes = np.arange(len(batidas))                  #Enumera todas as batidas
+simbolos = np.split(posicoes, diferente)            #cria lista agrupando cada batida onde intervalo aponta para duas batidas
+for i in simbolos:
+    if len(i) == 1:
+        resultado.append(0)                         #Nota que foi batida unica e adiciona 0 ao resultado
+    elif len(i) == 2:
+        resultado.append(1)                         #Nota que foi batida dupla e adiciona 1 ao resultado
+    else:
+        resultado.append(3)                         #Controle de erros... pode ser modificado
+print(resultado)                                    
